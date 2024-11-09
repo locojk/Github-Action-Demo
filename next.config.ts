f91da49e42e04737feeb23a15fbc9a7c45022ef3
Jun Gao <121655608+locojk@users.noreply.github.com>
2024-11-09 05:04:17 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,31 +13,17 @@
 //   },
 // };
 
-// export default nextConfig;
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig: import('next').NextConfig = {
 //   output: "standalone",
 // };
 
-// export default nextConfig;
-
-// next.config.ts
-
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  // Enables standalone mode, which makes the application suitable for containerization
-  output: "standalone",
-
-  // Static export setting
-  // Configures Next.js to export the site as static HTML files
-  exportTrailingSlash: true, // Adds a trailing slash to all paths, which is helpful for static exports
-  images: {
-    unoptimized: true, // Disables Next.js image optimization, required for static exports
-  },
-  trailingSlash: true, // Ensures URLs have trailing slashes for static files
+/** @type {import('next').NextConfig} */
+const nextConfig: import('next').NextConfig = {
+  output: "export",
+  reactStrictMode: true
 };
+
 
 export default nextConfig;
 
